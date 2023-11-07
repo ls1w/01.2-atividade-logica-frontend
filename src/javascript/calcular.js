61f0d245814aa2d1b0f75cc0c99d4083ae2287a9
ls1w <michelle_j.b@hotmail.com>
2023-11-07 09:16:59 -0300

    enunciado 2 ok

---
+++ src/javascript/calcular.js
@@ -0,0 +1,40 @@
+let tempo;
+let velocidade;
+let distancia;
+let consumo;
+
+function limpar(){
+    document.querySelector('#tempo').value = '';
+    document.querySelector('#velocidade').value = '';
+    document.querySelector('#distancia').innerHTML = '-';
+    document.querySelector('#consumo').innerHTML = '-';
+}
+
+
+function calcular(){
+    //calcularDistancia();
+    calcularConsumo();
+}
+
+
+
+function calcularDistancia(){
+    // document.querySelector('#distancia').innerHTML = 'test';
+    tempo = parseInt(document.querySelector('#tempo').value);
+    velocidade = parseInt(document.querySelector('#velocidade').value);
+
+    distancia = tempo * velocidade
+    document.querySelector('#distancia').innerHTML = (distancia + ' KM');
+    return distancia;
+}
+
+function calcularConsumo(){
+    // document.querySelector('#consumo').innerHTML = 'test2';
+
+    let distancia = calcularDistancia();
+
+    consumo = (distancia / 12);
+
+    document.querySelector('#consumo').innerHTML = (consumo + ' KM/L')
+    return consumo
+}
